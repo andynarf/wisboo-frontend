0290df7cfaafd2450cbc635057f7328b28cd6be8
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-10-31 02:14:25 -0300

    working on videos view, added flexbox and some styling in order to match images received as reference

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-import Video from './components/Video'
+import Video from "./components/Video";
 
 function App() {
-  const [videoList, setvideoList] = useState([])
+  const [videoList, setvideoList] = useState([
+    "vuw0ay27tr",
+    "vuw0ay27tr",
+    "vuw0ay27tr",
+    "vuw0ay27tr",
+    "vuw0ay27tr",
+    "vuw0ay27tr",
+  ]);
 
   return (
     <div className="App">
-      {/* {videoList.map(video => {
-        return (video)
-      })} */}
-      <Video id='vuw0ay27tr'></Video>
-      <Video id='vuw0ay27tr'></Video>
-      <Video id='vuw0ay27tr'></Video>
-      <Video id='vuw0ay27tr'></Video>
-      <Video id='vuw0ay27tr'></Video>
-      <Video id='vuw0ay27tr'></Video>
-      <Video id='vuw0ay27tr'></Video>
-
+      <div className='title__container'>
+        <h1> Libreria de videos</h1>
+      </div>
+      <div className="videos__container">
+        {videoList.map((video) => {
+          return <Video id={video}></Video>;
+        })}
+      </div>
     </div>
   );
 }
