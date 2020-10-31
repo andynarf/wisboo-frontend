662ba0c42624b32d681c3fa46e802f4b543d3c89
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-10-31 01:30:54 -0300

    created React App , created reusable Video component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import Video from './components/Video'
+
 function App() {
+  const [videoList, setvideoList] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {videoList.map(video => {
+        return (video)
+      })} */}
+      <Video id='vuw0ay27tr'></Video>
+      <Video id='vuw0ay27tr'></Video>
+      <Video id='vuw0ay27tr'></Video>
+      <Video id='vuw0ay27tr'></Video>
+      <Video id='vuw0ay27tr'></Video>
+      <Video id='vuw0ay27tr'></Video>
+      <Video id='vuw0ay27tr'></Video>
+
     </div>
   );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './Video.css'
+
+export default class Video extends React.Component {
+  componentWillMount() {
+    const script1 = document.createElement("script");
+    const script2 = document.createElement("script");
+
+    script1.src = `https://fast.wistia.com/embed/medias/${this.props.id}.jsonp`;
+    script1.async = true;
+
+    script2.src = "https://fast.wistia.com/assets/external/E-v1.js";
+    script2.async = true;
+
+    document.body.appendChild(script1);
+    document.body.appendChild(script2);
+  }
+
+  render() {
+    return (
+        <div className='video__contained'>
+            <div className= {`wistia_embed wistia_async_${this.props.id} videoFoam=true`}/>
+        </div>
+    );
+  }
+}
